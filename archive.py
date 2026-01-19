9cf27ba44da56927a812f15ded8d23a5149da3b6
Li Ying <121075683+yl25946@users.noreply.github.com>
2026-01-19 15:02:37 -0500

    Live Updates (#1)
    Will continually run until the actual archival process completes
    
    Will return true or false depending on if the program thinks the archival was successful
    
    Adds a debug flag. If you pass in true, then it will print out all the contents of all the requests

--- archive.py
+++ archive.py
@@ -2,11 +2,22 @@ import requests
 from dotenv import load_dotenv
 from dotenv import find_dotenv
 import os
+import time
+import random
 
-# Please only tiny websites, otherwise this might crassh
+
+def print_debug(r):
+    print("Status Code:", r.status_code)
+    print("Response JSON:", r.json())
+
+# Please only tiny websites, otherwise this might crash
+# Will return true if it was successsful, otherwise will return false
 
 
-def archive(link):
+def archive(link, debug):
+    # Starts system clock
+    start = time.time()
+
     # Loads in the ACCESS_KEY AND SECRET_KEY
     load_dotenv()
 
@@ -36,10 +47,56 @@ def archive(link):
     try:
         r = requests.post(url, data=payload, headers=headers)
         if r.ok:
-            print(f'Successfully archived {link}!')
+            job_id = r.json()["job_id"]
+
+            url = os.getenv('INTERNET_ARCHIVE_STATUS_CHECK_URL') + job_id
+
+            # continue looping until we get successful or fail
+            while True:
+                print(f'Archiving {link} ... ({time.time() - start:.1f}s)')
+
+                if debug:
+                    print_debug(r)
+
+                try:
+                    r = requests.get(url, headers=headers)
+
+                    if r.ok:
+                        response = r.json()
+
+                        if response['status'] == 'success':
+                            print(
+                                f'Finished archiving {link}! ({time.time() - start:.1f}s)')
+                            if debug:
+                                print_debug(r)
+                            return True
+
+                        elif response['status'] == 'pending':
+                            pass
+                        # something went wrong with the request, abort
+                        else:
+                            print(f"Error occured while archiving {link}!")
+                            print_debug(r)
+                            return False
+
+                except:
+                    print("Could not access server!")
+                    if debug:
+                        print_debug(r)
+                    return False
+
+                # pause time, wait a random time (20,25) so it seems like process is random
+                # also so we don't hit a rate limit, change numbers here if we think we hit a rate limit
+                pause_time = 20 + 5 * random.random()
+
+                time.sleep(pause_time)
+
         else:
             print(f'Could not archive {link}!')
-        print("Status Code:", r.status_code)
-        print("Response JSON:", r.json())
-    except:
-        print(f'Could not archive {link}')
+            print_debug(r)
+
+    except Exception as e:
+        print(f'Could not archive {link}!')
+        print('Error: ', e)
+
+    return False
